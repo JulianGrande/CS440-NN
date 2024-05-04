808fa0b747ab8a98fde96ed580b682bf0b191f1d
kennethaugusto <kenneth.augusto@outlook.com>
2024-05-03 22:10:26 -0400

    updated

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -38,41 +38,39 @@ class NeuralNetwork:
 
         epochs = 3
         for epoch in range(epochs):
+            nr_correct = 0
+            total_error = 0
+
             for img, label in zip(trainingData, trainingLabels):
-                pixel_vector = np.array(list(img.values()))
-                pixel_vector.shape +=(1,)
+                pixel_vector = np.array(list(img.values())).reshape(-1, 1)
 
-                label_vector = np.zeros(10)
+                label_vector = np.zeros((10, 1))
                 label_vector[label] = 1
-                label_vector.shape +=(1,)
 
-                # Forward Prop.
+                # Forward Propagation
                 h_pre = b_i_h + w_i_h @ pixel_vector
                 h = 1 / (1 + np.exp(-h_pre))
 
                 o_pre = b_h_o + w_h_o @ h
                 o = 1 / (1 + np.exp(-o_pre))
 
-                e = 1 / len(o) * np.sum((o - label) ** 2)
-                nr_correct += int(np.argmax(o) == np.argmax(label))
+                # Error and Accuracy Tracking
+                total_error += np.sum((o - label_vector) ** 2) / len(o)
+                nr_correct += int(np.argmax(o) == label)
 
+                # Backpropagation
+                delta_o = o - label_vector
+                w_h_o -= learn_rate * (delta_o @ h.T)
+                b_h_o -= learn_rate * delta_o
 
-                delta_o = o - label
+                delta_h = (w_h_o.T @ delta_o) * (h * (1 - h))
+                w_i_h -= learn_rate * (delta_h @ pixel_vector.T)
+                b_i_h -= learn_rate * delta_h
 
-                delta_o = o - label
-
-
-                w_h_o += -learn_rate * delta_o @ h.T
-
-                b_h_o += -learn_rate * delta_o
-
-
-                delta_h = w_h_o.T @ delta_o * (h * (1 - h))
-
-                w_i_h += -learn_rate * delta_h @ pixel_vector.T
-
-
-                b_i_h += -learn_rate * delta_h
+            # Print accuracy and average error for the epoch
+            accuracy = nr_correct / len(trainingData)
+            average_error = total_error / len(trainingData)
+            print(f"Epoch {epoch + 1}: Accuracy: {accuracy * 100:.2f}%, Avg Error: {average_error:.4f}")
 
 
 
